f2f591fa3e0404056755224bb747df72dbeabe3a
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2026-01-08 09:20:33 -0500

    Change in main.ts app.enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const frontendOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+  const frontendOrigin = process.env.FRONTEND_URL || 'http://localhost:5173' || "http://localhost:3000" ;
   
   app.enableCors({
-    origin: frontendOrigin,
+    origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   });
